38f06f03222518ee2d64f10f33c878edc70b1b08
Mgonand <marcos@binpar.com>
2018-09-18 17:45:44 +0200

    Fix

--- node-8/scripts/install_node_8.sh
+++ node-8/scripts/install_node_8.sh
@@ -21,17 +21,17 @@ for key in \
   gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "$key" || \
   gpg --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys "$key" || \
   gpg --keyserver hkp://pgp.mit.edu:80 --recv-keys "$key" ; \
-done \
-  && curl -fsSLO --compressed "https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION.tar.xz" \
-  && curl -fsSLO --compressed "https://nodejs.org/dist/$NODE_VERSION/SHASUMS256.txt.asc" \
-  && gpg --batch --decrypt --output SHASUMS256.txt SHASUMS256.txt.asc \
-  && grep " node-$NODE_VERSION.tar.xz\$" SHASUMS256.txt | sha256sum -c - \
-  && tar -xf "node-$NODE_VERSION.tar.xz" \
-  && cd "node-$NODE_VERSION" \
-  && ./configure \
-  && make -j$(getconf _NPROCESSORS_ONLN) \
-  && make install \
-  && apk del .build-deps \
-  && cd .. \
-  && rm -Rf "node-$NODE_VERSION" \
-  && rm "node-$NODE_VERSION.tar.xz" SHASUMS256.txt.asc SHASUMS256.txt
+done
+curl -fsSLO --compressed "https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION.tar.xz"
+curl -fsSLO --compressed "https://nodejs.org/dist/$NODE_VERSION/SHASUMS256.txt.asc"
+gpg --batch --decrypt --output SHASUMS256.txt SHASUMS256.txt.asc
+grep " node-$NODE_VERSION.tar.xz\$" SHASUMS256.txt | sha256sum -c -
+tar -xf "node-$NODE_VERSION.tar.xz"
+cd "node-$NODE_VERSION"
+./configure
+make -j$(getconf _NPROCESSORS_ONLN)
+make install
+apk del .build-deps
+cd ..
+rm -Rf "node-$NODE_VERSION"
+rm "node-$NODE_VERSION.tar.xz" SHASUMS256.txt.asc SHASUMS256.txt
